0640b892b0a10b465c1e06e8428f1a7bc04be0ac
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-12-29 13:04:55 -0300

    TODOs can be added trough the modal form that CreateTodoButton activates. Basic app is ready to be deployed on GitHub Pages now.

--- src/components/TodoForm/index.js
+++ src/components/TodoForm/index.js
@@ -1,23 +1,47 @@
-import './TodoForm.css' 
+import React from 'react';
+import './TodoForm.css';
+import {TodoContext} from '../TodoContext';
+
 
 function TodoForm(){
+    const {
+		addTodo,
+		setOpenModal,
+	}
+	= React.useContext(TodoContext);
+
+    const [newText, setNewText] = React.useState();
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setOpenModal(false);
+        addTodo(newText);
+     }
+     const onCancel = () => {
+         setOpenModal(false);
+     }
+     const onChange = (event) => {
+        setNewText(event.target.value);
+     }
 	return (
         <>
-            <form onSubmit={(event) =>{
-                event.preventDefault();
-            }}>
+            <form onSubmit={onSubmit}>
                 <label> Write your new TODO </label>
-                <textarea placeholder="Take Cookie out for a walk"
+                <textarea  onChange={onChange} 
+                value={newText}
+                placeholder="Take Cookie out for a walk"
+                required
                 />
                 <div className="TodoForm-container">
                     <button className="TodoForm-button TodoForm-button--cancel"
+                    onClick={onCancel}
                 >	Cancel</button>
                     <button className="TodoForm-button TodoForm-button--add"
+                    onSubmit={onSubmit}
                 >	Add</button>
                 </div>
             </form>    
         </>
-
 	);
 }
 
