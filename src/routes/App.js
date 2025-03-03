44d069fb2e918d84edf3189bd6209b46a40005a7
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-03-03 15:34:15 -0300

    feat(router): implement HashRouter and reorganize directories for home & new/edit TODO flows
    - Introduce HashRouter for navigation
    - Add /new and /edit/:id routes
    - Move related components into newly structured folders
    - Remove modal approach in favor of page-based creation/editing

---
+++ src/routes/App.js
@@ -0,0 +1,23 @@
+//src/App/index.js
+import './App.css';
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './home/HomePage.jsx';
+import { NewTodoPage } from './new/NewTodoPage';
+import { EditTodoPage } from './edit/EditTodoPage';
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/new" element={<NewTodoPage/>} />
+        <Route path="/edit/:id" element={<EditTodoPage/>} />
+        <Route path="/edit/:id" element={<EditTodoPage/>} />
+        <Route path="*" element={<p> Not Found :c </p>} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export {App};
